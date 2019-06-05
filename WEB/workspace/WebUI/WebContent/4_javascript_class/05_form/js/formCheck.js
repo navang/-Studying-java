8a1f227d82d499beb597912fde91d86036241df0
navang92 <2012102367@khu.ac.kr>
2019-06-05 18:53:35 +0900

    no message

--- WEB/workspace/WebUI/WebContent/4_javascript_class/05_form/js/formCheck.js
+++ WEB/workspace/WebUI/WebContent/4_javascript_class/05_form/js/formCheck.js
@@ -1,8 +1,28 @@
 window.onload = function(){
 
-		var frm = document.getElementById('frm');
+		//var frm = document.getElementById('frm');
+		//var inputs = document.querySelectorAll("input");
+		var frm = document.querySelector('#frm');
 		var inputs = document.querySelectorAll("input");
 		
+		frm.onsubmit = function(e){
+			e.preventDefault();
+			//alert("서버로");
+			
+		/*	var agree = frm.agree;
+			//alert(agree.checked)
+			if(!agree.checked) {
+				alert('ㅎ약관 확인 ㅎ');
+				return;
+			}*/
+			// 별칭이 ['babo','geni', 'idiot','gdb']
+		
+			var nickname = document.getElementById('nickname').value;
+			var baned = ['babo','geni', 'idiot','gdb'];
+			alert(baned);
+			
+			frm.submit();
+		}
 
 
 	}
